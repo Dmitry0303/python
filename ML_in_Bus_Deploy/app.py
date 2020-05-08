7f59435fb0c7210b436338d0afdce7d8121b1180
Dima <fds0303@mail.ru>
2020-05-08 14:19:41 +0300

    ML_in_Bus_Deploy_HW

---
+++ ML_in_Bus_Deploy/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+import h2o
+
+from process_data import process_input
+
+app = Flask(__name__)
+
+h2o.init()
+#h2o.init(ip='127.0.0.1')
+model_glm_poisson = h2o.load_model('./models/GLM_model_python_1573818197972_1')
+model_glm_gamma = h2o.load_model('./models/GLM_model_python_1573818197972_2')
+
+
+@app.route('/')
+def index():
+    return "API for predict servise"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    json_input = request.json
+
+    id = json_input['ID']
+    hf = process_input(json_input)
+
+    prediction_Poisson = model_glm_poisson.predict(hf)
+    value_Poisson = prediction_Poisson.as_data_frame()['predict'][0]
+
+    prediction_Gamma = model_glm_gamma.predict(hf)
+    value_Gamma = prediction_Gamma.as_data_frame()['predict'][0]
+
+    value_BurningCost = value_Poisson * value_Gamma
+
+    result = {
+        'ID': id,
+        'value_Poisson': value_Poisson,
+        'value_Gamma': value_Gamma,
+        'value_BurningCost': value_BurningCost
+    }
+
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
